ad070872bfa4a89fa679a7c8c02cbbc0f129acb6
Nikolas Trabue <ntrabue@preferredphm.com>
2018-04-06 12:12:31 -0400

    Implemented DataTables plugin on the Client Index Page

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -16,6 +16,7 @@ namespace Vidly.App_Start
 
             Mapper.CreateMap<Customer, CustomerDto>();
             Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MembershipType, MembershipTypeDto>();
 
             #endregion
 
--- Vidly/Controllers/API/CustomersController.cs
+++ Vidly/Controllers/API/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using AutoMapper;
+using System.Data.Entity;
 using Vidly.Data;
 using Vidly.Dtos;
 using Vidly.Models;
@@ -23,7 +24,10 @@ namespace Vidly.Controllers.API
         // GET /api/customers
         public IEnumerable<CustomerDto> GetCustomers()
         {
-            return _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
+            return _context.Customers
+                .Include(c => c.MembershipType)
+                .ToList()
+                .Select(Mapper.Map<Customer, CustomerDto>);
         }
 
         // GET /api/customers/1
